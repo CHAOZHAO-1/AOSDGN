500271638fcbb95b761b96c3a0b0cb9ddbce85e0
Zhao Chao <47875987+CHAOZHAO-1@users.noreply.github.com>
2024-11-14 19:04:43 +0800

    Update resnet18_1d.py

--- resnet18_1d.py
+++ resnet18_1d.py
@@ -39,3 +39,64 @@ class CNN_1D(nn.Module):
 
         return pre, feature
 
+
+class CNN(nn.Module):
+    def __init__(self, pretrained=False, in_channel=1, num_classes=10):
+        super(CNN, self).__init__()
+
+
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(1, 16, kernel_size=64,stride=1),  # 32, 24, 24
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            )  # 32, 12,12     (24-2) /2 +1
+
+
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(16, 32, kernel_size=16,stride=1),  # 128,8,8
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2))# 128, 4,4
+
+        self.layer3 = nn.Sequential(
+            nn.Conv1d(32, 64, kernel_size=5,stride=1),  # 32, 24, 24
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )  # 32, 12,12     (24-2) /2 +1
+
+        self.layer4 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=5,stride=1),  # 128,8,8
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveMaxPool1d(4)
+        )
+
+
+
+        # self.fc = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+
+        x = x.unsqueeze(1)
+        # print(x.shape)
+
+        x = self.layer1(x)
+        # print(x.shape)
+
+        x = self.layer2(x)
+        # print(x.shape)
+        x = self.layer3(x)
+        # print(x.shape)
+        x = self.layer4(x)
+        # print(x.shape)
+
+
+        x = x.view(x.size(0), -1)
+
+        # x = self.layer5(x)
+
+        # x = self.fc(x)
+
+        return x
